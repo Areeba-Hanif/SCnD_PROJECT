a685326d0b1d7c0a3693b1d3684172309c131c55
Areeba-Hanif <areeba.hanif222@gmail.com>
2024-01-07 19:23:39 +0500

    File Modified

--- ECommerceProject/src/main/RetrieveOrderInformation.java
+++ ECommerceProject/src/main/RetrieveOrderInformation.java
@@ -1,58 +1,30 @@
 package main;
 
-import java.sql.*;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
 
 public class RetrieveOrderInformation {
 
-	private int id;
-    private String date;
-    private String amount;
-    private String status;
+    public static Order getOrderInformation(int orderId) {
+    	
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+            Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/ecommercehubdb", "root", "");
+            Statement stmt = con.createStatement();
+            ResultSet rs = stmt.executeQuery("select * from Orders where OrderID = " + orderId);
 
-   
+            rs.next();
+            Order orderInfo = new Order(rs.getInt(1),rs.getString(2),rs.getString(3),rs.getString(4));
+            con.close();
+            return orderInfo;
+            
+        } catch (Exception e) {
+        	System.out.println(e);
+        }
 
-    public RetrieveOrderInformation(int id, String date, String amount, String status) {
-        this.id = id;
-        this.date = date;
-        this.amount = amount;
-        this.status = status;
+        return null; 
     }
-
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
-
-        this.id = id;
-    }
-
-    public String getDate() {
-        return date;
-    }
-
-    public void setDate(String date) {
-        this.date = date;
-    }
-
-    public String getAmount() {
-        return amount;
-    }
-
-    public void setAmount(String amount) {
-        this.amount = amount;
-    }
-
-    public String getStatus() {
-        return status;
-    }
-
-
-    public void setStatus(String status) {
-        this.status = status;
-    }
-
-  
-
-
 }
+
