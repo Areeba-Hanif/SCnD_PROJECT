651c1bcc6754d785c94067f34a128bd96b883331
Areeba-Hanif <areeba.hanif222@gmail.com>
2024-01-07 19:13:29 +0500

    SQL COMMITTED

--- ecommercehubdb.sql
+++ ecommercehubdb.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Dec 23, 2023 at 08:40 AM
+-- Generation Time: Jan 07, 2024 at 03:04 PM
 -- Server version: 10.4.27-MariaDB
 -- PHP Version: 8.2.0
 
@@ -33,6 +33,36 @@ CREATE TABLE `categories` (
   `Description` varchar(50) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
+--
+-- Dumping data for table `categories`
+--
+
+INSERT INTO `categories` (`CategoryID`, `CategoryName`, `Description`) VALUES
+(1, 'Spring Collection', 'Spring Clothes ');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `orderitem`
+--
+
+CREATE TABLE `orderitem` (
+  `OrderItemID` int(11) NOT NULL,
+  `OrderID` int(11) NOT NULL,
+  `ProductID` int(11) NOT NULL,
+  `Quantity` int(11) NOT NULL,
+  `SubTotal` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `orderitem`
+--
+
+INSERT INTO `orderitem` (`OrderItemID`, `OrderID`, `ProductID`, `Quantity`, `SubTotal`) VALUES
+(101, 101, 101, 3, 1000),
+(102, 102, 102, 2, 900),
+(103, 103, 3, 4, 600);
+
 -- --------------------------------------------------------
 
 --
@@ -45,6 +75,20 @@ CREATE TABLE `orders` (
   `TotalAmount` float NOT NULL,
   `Status` varchar(45) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `orders`
+--
+
+INSERT INTO `orders` (`OrderID`, `OrderDate`, `TotalAmount`, `Status`) VALUES
+(101, '2023-10-15', 1000, 'Shipped'),
+(102, '2023-11-12', 400, 'Pending'),
+(103, '2023-01-12', 5000, 'Shipped'),
+(104, '2022-12-04', 5000, 'Shipped'),
+(105, '2021-12-23', 20000, 'Pending'),
+(106, '2023-04-23', 7000, 'Pending'),
+(107, '2023-08-19', 2000, 'Shipped'),
+(108, '2023-12-10', 7000, 'Pending');
 
 -- --------------------------------------------------------
 
@@ -60,6 +104,17 @@ CREATE TABLE `products` (
   `Description` varchar(45) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
+--
+-- Dumping data for table `products`
+--
+
+INSERT INTO `products` (`ProductID`, `ProductName`, `Price`, `StockQuantity`, `Description`) VALUES
+(1, 'shirt', 500, 30, 'large,medium,small'),
+(2, 'skirt', 3000, 53, 'large,medium,small'),
+(3, 'jeans', 1000, 44, 'large,medium,small'),
+(101, 'Shirt', 500, 5, 'Simple Shirt'),
+(102, 'Trouser', 1000, 10, 'Black Colour');
+
 -- --------------------------------------------------------
 
 --
@@ -70,9 +125,17 @@ CREATE TABLE `users` (
   `UserID` int(11) NOT NULL,
   `Username` varchar(45) NOT NULL,
   `Password` varchar(45) NOT NULL,
-  `Email` varchar(45) NOT NULL,
-  `UserType` varchar(45) NOT NULL
+  `Email` varchar(45) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`UserID`, `Username`, `Password`, `Email`) VALUES
+(1, 'Areeba Hanif', '54321', 'areeba.hanif222@gmail.com'),
+(2, 'admin', '12345', 'admin111@gmail.com'),
+(5, 'User1', '111000aaa', 'user111@gmail.com');
 
 --
 -- Indexes for dumped tables
@@ -83,6 +146,14 @@ CREATE TABLE `users` (
 --
 ALTER TABLE `categories`
   ADD PRIMARY KEY (`CategoryID`);
+
+--
+-- Indexes for table `orderitem`
+--
+ALTER TABLE `orderitem`
+  ADD PRIMARY KEY (`OrderItemID`),
+  ADD KEY `OrderID` (`OrderID`),
+  ADD KEY `ProductID` (`ProductID`);
 
 --
 -- Indexes for table `orders`
@@ -110,25 +181,42 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT for table `categories`
 --
 ALTER TABLE `categories`
-  MODIFY `CategoryID` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `CategoryID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `orderitem`
+--
+ALTER TABLE `orderitem`
+  MODIFY `OrderItemID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=104;
 
 --
 -- AUTO_INCREMENT for table `orders`
 --
 ALTER TABLE `orders`
-  MODIFY `OrderID` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `OrderID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=109;
 
 --
 -- AUTO_INCREMENT for table `products`
 --
 ALTER TABLE `products`
-  MODIFY `ProductID` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `ProductID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=103;
 
 --
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `UserID` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `UserID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `orderitem`
+--
+ALTER TABLE `orderitem`
+  ADD CONSTRAINT `orderitem_ibfk_1` FOREIGN KEY (`OrderID`) REFERENCES `orders` (`OrderID`),
+  ADD CONSTRAINT `orderitem_ibfk_2` FOREIGN KEY (`ProductID`) REFERENCES `products` (`ProductID`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
